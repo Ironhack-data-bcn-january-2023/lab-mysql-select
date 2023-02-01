6c2aa125726bd9e3393384687f027d3d262b7aec
German Ortola <germanortola@gmail.com>
2023-02-01 19:03:24 +0100

    w3 d3 sql select

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,6 +1,6 @@
 use publications;
 
-select au_id as author_id, au_lname as last_name, au_fname as first_name, title, pub_name as publisher
+select authors.au_id as "AUTHOR ID", au_lname as "LAST NAME", au_fname as "FIRST NAME", title as "TITLE", pub_name as "PUBLISHER"
 	from authors
 	join titleauthor
 		on authors.au_id = titleauthor.au_id
@@ -8,4 +8,34 @@ select au_id as author_id, au_lname as last_name, au_fname as first_name, title,
 		on titleauthor.title_id = titles.title_id
 	join publishers
 		on titles.pub_id = publishers.pub_id
-	
+    order by authors.au_id;
+    
+select authors.au_id as "AUTHOR ID", au_lname as "LAST NAME", au_fname as "FIRST NAME", pub_name as "PUBLISHER", count(titles.title) as "COUNT"
+	from authors
+	join titleauthor
+		on authors.au_id = titleauthor.au_id
+	join titles
+		on titleauthor.title_id = titles.title_id
+	join publishers
+		on titles.pub_id = publishers.pub_id
+	group by authors.au_id, publishers.pub_id
+    order by authors.au_id;
+    
+select authors.au_id as "AUTHOR ID", au_lname as "LAST NAME", au_fname as "FIRST NAME", SUM(qty) as "TOTAL"
+	from authors
+	join titleauthor
+		on authors.au_id = titleauthor.au_id
+	join sales
+		on titleauthor.title_id = sales.title_id
+	group by authors.au_id
+    order by TOTAL desc
+    limit 3;
+    
+select authors.au_id as "AUTHOR ID", au_lname as "LAST NAME", au_fname as "FIRST NAME", ifnull(SUM(qty), 0) as "TOTAL"
+	from authors
+	left join titleauthor
+		on authors.au_id = titleauthor.au_id
+	left join sales
+		on titleauthor.title_id = sales.title_id
+	group by authors.au_id
+    order by TOTAL desc;
